4fb14db7b978aa5c2191c463f550ce81842f5386
Radoslaw Drabczyk <radoslaw.drabczyk@dxc.com>
2024-01-18 16:52:21 +0100

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,7 +5,7 @@ import RadoComponent from '../components/Rado'
 const HomePage: React.FC = () => {
     return (
         <div style={{ position: 'relative', width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
-            <h1 style={{ fontSize: '4em' }}>Hello world!</h1>
+            <h1 style={{ fontSize: '4em' }}>Hello world!!!</h1>
             <DateDisplay />
             <RadoComponent text="RADO" />
         </div>
